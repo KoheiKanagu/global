060e10d23b8b64b58a249fb3e3fae60f6060df69
KoheiKanagu <kanagu@kingu.dev>
2022-09-05 09:57:28 +0900

    update

--- dotfiles/google-chrome-unsafe.sh
+++ dotfiles/google-chrome-unsafe.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
 
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --disable-web-security --user-data-dir="$HOME/Documents/unsafe-chrome" "$*"
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --disable-web-security $*
